d88ca8f71c9f958b809526b409ada66143b16769
Rochan543 <premrochan@gmail.com>
2025-03-24 14:00:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+# Home Route
+@app.get("/")
+def home():
+    return {"message": "Hello, FastAPI!"}
+
+# About Page
+@app.get("/about")
+def about():
+    return {"info": "This is a basic FastAPI application."}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
